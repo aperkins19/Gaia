b65a9402a00a09f3b0ac70e12b967387aa87cedf
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-12-17 21:22:54 +0000

    led pin check

---
+++ moisure_logger.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO  # interact with Pis pins
+import time
+
+# setup pins
+GPIO.setmode(GPIO.DCM)
+GPIO.setup(18, GPIO.OUT)
+
+# run
+GPIO.output(18, GPIO.LOW)
+
+for i in range(0, 100, 1):
+
+    GPIO.output(18, GPIO.HIGH)
+
+    time.sleep(2)
+
+    GPIO.output(18, GPIO.LOW)
+
+    time.sleep(2)
+
+GPIO.cleanup()
