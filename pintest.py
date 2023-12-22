543611d3c247237b32bf6b6d12cfae7ef4554486
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-12-22 16:40:31 +0000

    add pintest

---
+++ pintest.py
@@ -0,0 +1,19 @@
+mport RPi.GPIO as GPIO
+import time
+
+# Set the GPIO mode
+GPIO.setmode(GPIO.BCM)
+
+# Set up the GPIO pin for output
+pin_to_test = 2  # Change to 3 for GPIO 3 test
+GPIO.setup(pin_to_test, GPIO.OUT)
+
+# Blink the LED
+try:
+    while True:
+        GPIO.output(pin_to_test, GPIO.HIGH)  # LED on
+        time.sleep(1)
+        GPIO.output(pin_to_test, GPIO.LOW)   # LED off
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()  # Clean up GPIO on CTRL+C exit
