30d911d7058a0b7719f1d8e27de8d437f698d1ed
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-12-24 15:02:01 +0000

    init CSMS connection

--- moisure_logger.py
+++ moisure_logger.py
@@ -1,19 +1,31 @@
 import time
-import board
-import busio
 
-import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+import Adafruit_ADS1x15
 
 # Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
+adc = Adafruit_ADS1x15.ADS1115()
 
-# Create the ADS1115 ADC instance
-ads = ADS.ADS1115(i2c)
+GAIN = 1
 
-# Create an analog input channel on the ADS1115
-chan = AnalogIn(ads, ADS.P0) # Assuming you're using channel 0
-
+print('Reading ADS1x15 values, press Ctrl-C to quit...')
+# Print nice channel column headers.
+print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
+print('-' * 37)
+# Main loop.
 while True:
-    print("Moisture Level:", chan.value, "Raw ADC Value:", chan.voltage)
-    time.sleep(1)
+    # Read all the ADC channel values in a list.
+    values = [0]*4
+    for i in range(4):
+        # Read the specified ADC channel using the previously set gain value.
+        values[i] = adc.read_adc(i, gain=GAIN)
+        # Note you can also pass in an optional data_rate parameter that controls
+        # the ADC conversion time (in samples/second). Each chip has a different
+        # set of allowed data rate values, see datasheet Table 9 config register
+        # DR bit values.
+        #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
+        # Each value will be a 12 or 16 bit signed integer value depending on the
+        # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
+    # Print the ADC values.
+    print('| {0:>6`enter code here`} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
+    # Pause for half a second.
+    time.sleep(0.5) 
