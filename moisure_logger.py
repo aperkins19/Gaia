b0647a883f07d63e1a797932656273b286ccb600
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-12-24 15:32:09 +0000

    init CSMS connection

--- moisure_logger.py
+++ moisure_logger.py
@@ -1,18 +1,7 @@
 import time
-import board
-import busio
-import adafruit_ads1x15.ads1015 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+import Adafruit_ADS1x15
 
 # Create the I2C bus
-i2c = busio.I2C(board.SCL, board.SDA)
-
-# Create the ADS1015 ADC instance
-ads = ADS.ADS1015(i2c)
-
-# Create an analog input channel on the ADS1015
-chan = AnalogIn(ads, ADS.P0) # Assuming you're using channel 0
-
-while True:
-    print("Moisture Level:", chan.value, "Raw ADC Value:", chan.voltage)
-    time.sleep(1)
+adc = Adafruit_ADS1x15.ADS1115()
+GAIN=1
+value = adc.read_adc(0, gain=GAIN)
