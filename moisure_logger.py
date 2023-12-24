c6a91efda39b5230b48bdaf2ce2c1144c0cc5984
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2023-12-24 15:10:18 +0000

    init CSMS connection

--- moisure_logger.py
+++ moisure_logger.py
@@ -1,31 +1,41 @@
 import time
-
-import Adafruit_ADS1x15
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
 # Create the I2C bus
-adc = Adafruit_ADS1x15.ADS1115()
+i2c = busio.I2C(board.SCL, board.SDA)
 
-GAIN = 1
+# Create the ADS1015 ADC instance
+ads = ADS.ADS1015(i2c)
 
-print('Reading ADS1x15 values, press Ctrl-C to quit...')
-# Print nice channel column headers.
-print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
-print('-' * 37)
-# Main loop.
+# Create an analog input
+If the component you are using is an ADS1015 instead of an ADS1115, there are some differences you need to account for in your setup and code. Both are analog-to-digital converters from the same family but have different resolutions: the ADS1015 is a 12-bit ADC, while the ADS1115 is a 16-bit ADC.
+
+Here are the steps to adjust your setup for an ADS1015:
+
+### 1. **Update Your Python Script**
+Since the ADS1015 has a different resolution, you should ensure that your Python script accounts for this. The good news is that the Adafruit CircuitPython library for the ADS1x15 series supports both the ADS1015 and ADS1115, but you may need to adjust the code slightly.
+
+For an ADS1015, your script might look like this:
+
+```python
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADS1015 ADC instance
+ads = ADS.ADS1015(i2c)
+
+# Create an analog input channel on the ADS1015
+chan = AnalogIn(ads, ADS.P0) # Assuming you're using channel 0
+
 while True:
-    # Read all the ADC channel values in a list.
-    values = [0]*4
-    for i in range(4):
-        # Read the specified ADC channel using the previously set gain value.
-        values[i] = adc.read_adc(i, gain=GAIN)
-        # Note you can also pass in an optional data_rate parameter that controls
-        # the ADC conversion time (in samples/second). Each chip has a different
-        # set of allowed data rate values, see datasheet Table 9 config register
-        # DR bit values.
-        #values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
-        # Each value will be a 12 or 16 bit signed integer value depending on the
-        # ADC (ADS1015 = 12-bit, ADS1115 = 16-bit).
-    # Print the ADC values.
-    print('| {0:>6`enter code here`} | {1:>6} | {2:>6} | {3:>6} |'.format(*values))
-    # Pause for half a second.
-    time.sleep(0.5) 
+    print("Moisture Level:", chan.value, "Raw ADC Value:", chan.voltage)
+    time.sleep(1)
