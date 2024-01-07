bcd255772e205a315a0338fd6226cd7f0840d4bb
Alex Perkins <a.j.p.perkins@sms.ed.ac.uk>
2024-01-07 18:52:33 +0000

    mosfet wired up

--- module_script/module_script.ino
+++ module_script/module_script.ino
@@ -2,15 +2,19 @@
 #include <SD.h>
 #include <SPI.h>
 
+// calibration values;
+int soilMoistureThreshold = 350;
+
 // Declare 'datafile' globally
 File datafile;
 
 String datafileName = "testlog.csv";
-String initCSVColumnsLine = "Timestamp, Soil_Moisture_%, Light";
+String initCSVColumnsLine = "Timestamp, Soil_Moisture_%, Light, Pump_Running";
 
 // pins
 int soilMoistureSensorPin = A0; // Analog input pin that the sensor is attached to
 int photoResistorPin = A1;
+int mosfetSignalPin = 44; // Digital pin 44
 
 // SD card pins
 int sdChipSelectPin = 53;
@@ -18,13 +22,31 @@ int sdChipSelectPin = 53;
 // initialise data values
 int soilMoistureValue = 0;      // Value read from the sensor
 int lightVoltageValue = 0;
+bool pumpRunning = false;
 
 bool debugMode = true;
+
+// declare functions
+void runPump() {
+    // run pump
+    digitalWrite(mosfetSignalPin, HIGH);
+    delay(2000);
+    digitalWrite(mosfetSignalPin, LOW);
+    // set pumpRunning
+    pumpRunning = true;
+    
+  }
 
 void setup() {
   // pin setup
   pinMode(soilMoistureSensorPin, INPUT);
   pinMode(photoResistorPin, INPUT);
+  pinMode(mosfetSignalPin, OUTPUT);
+
+  // set mosfetSignalPin to LOW
+  digitalWrite(mosfetSignalPin, LOW);
+
+
   
   if (debugMode) {
     Serial.begin(9600);         // Start the serial communication    
@@ -66,16 +88,28 @@ void loop() {
   soilMoistureValue = analogRead(soilMoistureSensorPin); 
   lightVoltageValue = analogRead(photoResistorPin);
 
+  // mosfet control
+  // if the soilmoister is under the threshold, execute the function, else: set the run pump variable to false
+  if (soilMoistureValue > soilMoistureThreshold) {
+    runPump();
+    }
+  else {
+    pumpRunning = false;
+    }
+
   // Print the value to the serial monitor
   if (debugMode) {
-    Serial.print("Soil Moisture = ");
-    Serial.println(soilMoistureValue);
+    Serial.println(" ");
     Serial.print("Light = ");
     Serial.println(lightVoltageValue);
+    Serial.print("Soil Moisture = ");
+    Serial.println(soilMoistureValue);
+    Serial.print("Running Pump: ");
+    Serial.println(pumpRunning);
   }
 
   // Construct CSV data row
-  String datarow = String(millis()) + "," + String(soilMoistureValue) + "," + String(lightVoltageValue);
+  String datarow = String(millis()) + "," + String(soilMoistureValue) + "," + String(lightVoltageValue) + "," + String(pumpRunning);
 
   // write data to sd card
   datafile = SD.open(datafileName, FILE_WRITE); // open file for appending
@@ -90,5 +124,6 @@ void loop() {
     }
   }
 
-  delay(10000); // Delay in between reads for stability
+
+  delay(5000); // Delay in between reads for stability
 }
